5dee743cf162ca2b654cea2b587f22208f3caeca
LAPTOP-ULCU732A\User <valeri.ignateva@list.ru>
2023-05-22 22:00:15 +0300

    ll

---
+++ Matrix.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix6Lab
+{
+    internal class Matrix
+    {
+    }
+}
